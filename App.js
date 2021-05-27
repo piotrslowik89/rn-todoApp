75ffa7b7c530510e18a812a4b6283e6c023f1727
piotr slowik <piotrslowik409@gmail.com>
2021-05-27 11:48:47 +0200

    Initial

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  var list = [
+    { id: "1", text: "Lorem" },
+    { id: "2", text: "Ipsum" },
+    { id: "3", text: "Dolor" },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{marginTop:20, height:'100%'}}>
+      <FlatList data={list}
+        renderItem={(position) => {
+          return (
+            <View key={position.item.id}
+            style={{width:'100%', padding:15, borderButton:'1px',borderBottomColor:'black',flexDirection:'row'}}>
+             
+             <View style={{width:'80%'}}>
+               <Text style={{fontSize:20, alignSelf:'center'}}>{position.item.text}</Text>
+               </View>
+               <View style={{width:'20%'}}>
+                 <Text style={{fontSize:20,alignSelf:'center'}}>X</Text>
+                 </View>
+            </View>
+          );
+        }}
+      />
+
     </View>
   );
 }
@@ -14,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
